a75888045e9b1a16b5f5bdd90df9ae08247e4361
Felix Haußer <fhs@dotsource.de>
2024-09-20 15:10:18 +0200

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('renders App without crashing', () => {
+  render(<App />);
+});
+
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
